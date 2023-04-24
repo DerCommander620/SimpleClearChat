090ebd03d31a6ac3eeb4f64950de5b13b40f3af0
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-04-24 15:26:43 +0200

    Update Clearchat.php

--- src/DerCommander610/Clear/Clearchat.php
+++ src/DerCommander610/Clear/Clearchat.php
@@ -16,8 +16,8 @@ class Clearchat extends PluginBase {
     }
 
         public function onCommand(CommandSender $sender, Command $command, string $label, array $args): void {
-            if($command->getName()){
-                case "clearchat":
+            switch($command->getName()){
+              case "clearchat":
                   if($sender->hasPermission("clearchat.cmd")){
                     $player->sendMessage(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n" . $this->getConfig()->get("Prefix") . "§a >> Du hast erfolgreich den Chat gelöscht!");
                 }
