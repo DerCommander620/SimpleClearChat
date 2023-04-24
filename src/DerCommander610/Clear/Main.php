df7c5ceeb785f0e8807a68a344712d524a123c71
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-04-24 15:34:55 +0200

    Update Main.php

--- src/DerCommander610/Clear/Main.php
+++ src/DerCommander610/Clear/Main.php
@@ -3,7 +3,7 @@
 namespace DerCommander610\Clear;
 
 use pocketmine\player\Player;
-use pocketmine\command\CommanderSender;
+use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 
@@ -15,11 +15,11 @@ class Main extends PluginBase {
         $this->getResource("config.yml");
     }
 
-        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
             switch($command->getName()){
               case "clearchat":
                   if($sender->hasPermission("clearchat.cmd")){
-                    $sender->sendMessage(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n" . $this->getConfig()->get("Prefix") . "§a >> Du hast erfolgreich den Chat gelöscht!");
+                    $player->sendMessage(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n" . $this->getConfig()->get("Prefix") . "§a >> Du hast erfolgreich den Chat gelöscht!");
                 }
         }
     }
